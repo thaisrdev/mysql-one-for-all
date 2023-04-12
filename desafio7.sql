3075c98d210b4ef31726ab750524f70953cd35d2
Thais Rosa <tsrosa18@gmail.com>
2023-04-12 00:38:04 -0300

    publicando o projeto

---
+++ desafio7.sql
@@ -0,0 +1,10 @@
+-- SELECT 
+-- artists.artist_name AS artista,
+-- albuns.album_name AS album,
+-- COUNT(following.artist_id) AS seguidores
+-- FROM 
+-- SpotifyClone.artists AS artist 
+-- RIGHT JOIN 
+-- SpotifyClone.albuns AS albuns ON artists.artist_id = albuns.artist_id 
+-- INNER JOIN 
+-- SpotifyClone.fo
